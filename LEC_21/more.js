a1bfe199f899e9959d16f8f82dc62e77247f15a4
ypramod25 <py999618@gmail.com>
2025-01-13 17:40:24 +0530

    helllll

---
+++ LEC_21/more.js
@@ -0,0 +1,7 @@
+const email = "py@gmail.com";
+if(email) {
+    console.log("Enter password");
+}
+else {
+    console.log("Enter the email")
+}
---
+++ LEC_22/functions.js
@@ -0,0 +1,5 @@
+function calculateSumOfThreeNumbers(num1, num2, num3)  {
+    return num1 + num2 + num3;
+}
+let result = calculateSumOfThreeNumbers(1,2,3);
+console.log(result);
